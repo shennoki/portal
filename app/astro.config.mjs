062d8f4e53612975396e2596b359f06f6f1862fa
shinki <53631280+shennoki@users.noreply.github.com>
2024-03-22 00:41:26 +0900

    feat: wip

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,5 +2,19 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shinki.net'
+  site: 'https://shinki.net',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:`
+            @import "@styles/destyle.scss"; \
+            @import "@styles/mixins.scss"; \
+            @import "@styles/variables.scss"; \
+            @import "@styles/global.scss";
+          `,
+        }
+      }
+    }
+  },
 });
