fd5e857c0d35be4a9eccc982226245353596570d
shinki <53631280+shennoki@users.noreply.github.com>
2026-07-05 12:19:03 +0900

    build: upgrade to Astro 7 / TypeScript 6 / modern Sass API

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,17 +1,24 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shinki.net',
   vite: {
+    resolve: {
+      alias: {
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      },
+    },
     css: {
       preprocessorOptions: {
         scss: {
+          api: 'modern-compiler',
           additionalData:`
-            @import "@styles/_destyle.scss"; \
-            @import "@styles/_mixins.scss"; \
-            @import "@styles/_variables.scss"; \
-            @import "@styles/_global.scss";
+            @use "@styles/_destyle.scss" as *; \
+            @use "@styles/_mixins.scss" as *; \
+            @use "@styles/_variables.scss" as *; \
+            @use "@styles/_global.scss" as *;
           `,
         }
       }
